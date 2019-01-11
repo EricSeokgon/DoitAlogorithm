d9164e83321b60873082515aa808b3c701aa9cec
hadesyi <leesk55@gmail.com>
2019-01-11 13:14:15 +0900

    배열의 요솟값을 초기화하며 배열 선언하기

---
+++ src/chapter2/IntArrayInit.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class IntArrayInit {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+    }
+}

b740b89f0eb35e6049d7b9b518072348ac14fd54
hadesyi <leesk55@gmail.com>
2019-01-11 17:39:31 +0900

    스택 만들기 테스트

--- src/chapter4/IntStackTester.java
+++ src/chapter4/IntStackTester.java
@@ -9,7 +9,7 @@ public class IntStackTester {
         IntStack s = new IntStack(64);
 
         while (true) {
-            System.out.println("현재 데이터 수 : " + s.size() + " / "+s.capacity());
+            System.out.println("현재 데이터 수 : " + s.size() + " / " + s.capacity());
             System.out.print("(1)푸시  (2)팝  (3)피크  (4)덤프  (0)종료");
 
             int menu = stdIn.nextInt();
@@ -22,8 +22,30 @@ public class IntStackTester {
                     System.out.print("데이터 : ");
                     x = stdIn.radix();
                     try {
-                        s.push(x)
+                        s.push(x);
+                    } catch (IntStack.OverflowIntStackException e) {
+                        System.out.println("스택이 가득 찼습니다.");
                     }
+                    break;
+                case 2:
+                    try {
+                        x = s.pop();
+                        System.out.println("팝한 데이터는 " + x + "입니다.");
+                    } catch (IntStack.EmptyIntStackException e) {
+                        System.out.println("스택이 비어 있습니다.");
+                    }
+                    break;
+                case 3:
+                    try {
+                        x = s.peek();
+                        System.out.println("피크한 데이터는 " + x + "입니다.");
+                    } catch (IntStack.EmptyIntStackException e) {
+                        System.out.println("스택이 비어 있습니다.");
+                    }
+                    break;
+                case 4:
+                    s.dump();
+                    break;
             }
         }
     }

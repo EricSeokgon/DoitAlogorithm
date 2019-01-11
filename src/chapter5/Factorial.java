c7dac5a5ab1a94472f4f4fdf66019486c9869f41
hadesyi <leesk55@gmail.com>
2019-01-12 07:18:01 +0900

    팩토리얼

--- src/chapter5/Factorial.java
+++ src/chapter5/Factorial.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Factorial {
     static int factorial(int n) {
-        if (n > 0) {
+        /*if (n > 0) {
             return n * factorial(n - 1);
         } else {
             return 1;
-        }
+        }*/
+
+        return n > 0 ? n * factorial(n - 1) : 1;
     }
 
     public static void main(String[] args) {
